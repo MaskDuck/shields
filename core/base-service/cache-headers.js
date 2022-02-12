151896f16ce596efc69015d110fa96f015970e54
MaskDuck <i-am@maskduck.ninja>
2022-02-12 15:42:39 +0700

    Update cache-headers.js

--- core/base-service/cache-headers.js
+++ core/base-service/cache-headers.js
@@ -65,7 +65,7 @@ function setHeadersForCacheLength(res, cacheLengthSeconds) {
     cacheControl = 'no-cache, no-store, must-revalidate'
     expires = nowGMTString
   } else {
-    cacheControl = `max-age=$3, s-maxage=3`
+    cacheControl = `max-age=1, s-maxage=1`
     expires = new Date(now.getTime() + cacheLengthSeconds * 1000).toGMTString()
   }
 
